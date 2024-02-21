461620a09fa47979fe53f7163318fb8f9a29c243
棚橋沙紀子 <tanahashisakiko@tanahashisakikonoMacBook-Air.local>
2024-02-21 15:35:27 +0900

    test_frameworkに変更

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module V3BasicRailsBasic
       # helperファイルの設定
       g.helper false
       # testファイルの設定
-      g.test false
+      g.test_framework false
       # ルーティングの記述の設定
       g.skip_routes true
     end
