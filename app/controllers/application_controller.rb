335dca92a6d4130d2d7186f086874f1830bb82f7
棚橋沙紀子 <tanahashisakiko@tanahashisakikonoMacBook-Air.local>
2024-02-28 15:24:57 +0900

    sorceryを使ったユーザー登録機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :require_login
+  before_action :require_login
 
   private
-    def require_login
-      unless logged_in?
-        flash[:error] = "このセクションにアクセスするにはログインが必要です"
-        redirect_to new_login_url # リクエストサイクルを停止する
-      end
-    end
 
-    def not_authenticated
-        redirect_to login_path
+  def require_login
+    unless logged_in?
+      flash[:error] = "このセクションにアクセスするにはログインが必要です"
+      redirect_to new_login_url # リクエストサイクルを停止する
     end
+  end
+
+  def not_authenticated
+    redirect_to login_path
+  end
 
 end
