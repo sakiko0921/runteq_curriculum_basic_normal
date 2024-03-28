22e407ceeed4beba751c98a4153caa7bf4a21112
sakiko0921 <yyy>
2024-03-28 16:44:45 +0900

    コメント削除機能の追加とajax化

--- db/migrate/20240308132626_add_data_setting.rb
+++
@@ -1,8 +0,0 @@
-class AddDataSetting < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :boards, :title, false,0
-    change_column_null :boards, :body, false,0
-
-    change_column :boards, :users_id, :bigint, foreign_key: true
-  end
-end
