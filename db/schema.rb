1f310992bef69eab8c3d9a2e4906d4cd2c6a8163
sakiko0921 <yyy>
2024-04-02 23:06:25 +0900

    パスワードリセット機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_01_151848) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_02_140357) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_01_151848) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "unique_index_users_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
